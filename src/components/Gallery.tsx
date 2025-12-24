5fa45980eb24c22a8f801184bad999deeac9817f
dusannosovic <39407532+dusannosovic@users.noreply.github.com>
2025-12-24 17:09:02 +0100

    fix gallery

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -108,7 +108,7 @@ export default function Gallery() {
                     {post.images?.map((img) => (
                       <SwiperSlide key={img.id}>
                         <img 
-                          src={`${STRAPI_URL}${img.url}`} 
+                          src={`${img.url}`} 
                           alt={img.alternativeText || "Diving Gallery"} 
                           className="w-full h-full object-cover" 
                         />
